cc0749fdf4bef0d691aa1cae4523e250deed315a
niikun <niikun0209@gmail.com>
2026-08-10 22:33:11 +0900

    fix quantity_share

--- mpc-rust/src/auction.rs
+++ mpc-rust/src/auction.rs
@@ -1,25 +1,69 @@
 use crate::secret_sharing::{Fp, Share, split_into_shares, reconstruct};
 
-const PRICES: [u64; 5] = [100, 200, 300, 400, 500];
+const PRICES: [u64; 9] = [95,100,105,110,115,120,125,130,135];
 
-#[derive(Debug, Clone, Copy)]
-struct Quantity {
-    quantity: [Fp; 5],
+#[derive(PartialEq, Eq,Debug,Clone, Copy)]
+struct Quantity{
+    quantity:[Fp;9]
 }
+
 impl Quantity {
-    fn new(quantity: [Fp; 5]) -> Self {
-        Quantity { quantity }
+    fn new(quantity: [Fp; 9]) -> Self {
+        Quantity { quantity:quantity }
     }
 
-    fn from_quantities(quantities: [u64; 5]) -> Self {
-        let quantity = quantities.map(|q| Fp::new(q));
+    fn from_quantities(quantities: &[u64; 9]) -> Self {
+        let quantity:[Fp;9] = quantities.map(|q| Fp::new(q));
         Quantity::new(quantity)
     }
 
-    fn to_quantities(&self) -> [u64; 5] {
-        self.quantity.map(|bit| bit.value())
+    fn to_quantities(&self) -> [u64; 9] {
+        self.quantity.map(|bit:Fp| bit.value())
     }
 }
 
-pub fn set_quantities
+pub fn quantities_share(quantities:Quantity)->[[Fp;3];9]{
+    let quantities_shares = quantities.quantity.map(|q:Fp| split_into_shares(q));
+    quantities_shares
+}
+
+
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+    use rand::Rng;
+
+    #[test]
+    fn test_quantity(){
+        let quantities = [
+            Fp::new(10),Fp::new(10),Fp::new(10),Fp::new(10),Fp::new(10),Fp::new(10),Fp::new(10),Fp::new(10),Fp::new(10)
+            ];
+        let q1 = Quantity::new(quantities);
+        let q2 = Quantity::from_quantities(
+            &[10,10,10,10,10,10,10,10,10]
+        );
+        let quantity2 = q2.to_quantities();
+        assert_eq!(q1,q2);
+        assert_eq!(quantity2,[10,10,10,10,10,10,10,10,10]);
+    }
+
+    #[test]
+    fn test_quantities_share(){
+        let quantities = [
+            Fp::new(10),Fp::new(10),Fp::new(10),Fp::new(10),Fp::new(10),Fp::new(10),Fp::new(10),Fp::new(10),Fp::new(10)
+            ];
+        let q = Quantity::new(quantities);
+        let shares = quantities_share(q);
+        let mut count = 0;
+        for share in shares{
+            for s in share{
+                count += 1;
+            }
+        }
+        assert_eq!(27, count);
+    }
+
+}
+
 
